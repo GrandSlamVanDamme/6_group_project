c09d684303a9b87c02dfc6f071009e26b1b331b3
CS42 <278470006+GrandSlamVanDamme@users.noreply.github.com>
2026-07-23 22:21:58 -0600

    up to IV

--- 6_project_code.py
+++ 6_project_code.py
@@ -2,19 +2,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sci
 
+RNG = np.random.default_rng()  # returns random n ϵ
 
-def hypergeo_info(x, N, k, n):
+x_LIST = [np.arange(0, 21), np.arange(0, 31), np.arange(0, 41)]
+N = 200  # 200 bulbs per batch
+k_LIST = [2, 4, 6]  # 1%, 2%, 3% proportion of bad bulbs in a batch
+n_LIST = [20, 30, 40]  # bulbs per test sample
 
-    for ni in n:
-        for xi, ki in zip(x, k):
-            P = sci.stats.hypergeom.pmf(xi, N, ki, ni)
-            Ex = sci.stats.hypergeom.mean(xi, N, ki, ni)
-            Vx = sci.stats.hypergeom.var(xi, N, ki, ni)
-            print(
-                f"For a sample of {ni} bulbs, given {ki} defective bulbs in the batch, we report an expected value of {Ex} bulbs and a variance of {Vx}."
-            )
+# We'll assume a missed bad bulb costs the company 100X that of testing a single bulb.
+
+COST_PER_BULB_TESTED = 0.20  # 20 cents chosen arbitrarily
+COST_PER_BAD_BULB_MISSED = (
+    100 * COST_PER_BULB_TESTED
+)  # speculating based on word of mouth and loss of business over time.
+
+
+def hypergeo_info(x_LIST, N, k_LIST, n_LIST):
+    """
+    x = list of random vars x1, x2, ... xn where each xi is a range from 0 to ni
+    N = batch/population
+    k = list of numbers of successes per batch for different error proportions
+    n = list of sample sizes
+    """
+
+    x = x_LIST
+    k = k_LIST
+    n = n_LIST
+
+    for ki in k:
+        print()
+        print(f"########## With {ki} defective bulbs in the batch ##########")
+        print()
+        for xi, ni in zip(x, n):
+            hg = sci.stats.hypergeom(N, ki, ni)
+            P = hg.pmf(xi)
+            Ex = hg.mean()
+            Vx = hg.var()
+
+            # P_3_or_more_found
+            # P_2_or_more_found
+            # P_1_or_more_found
+
+            print()
+            print(f"Testing {ni} bulbs from the batch of 200 gives us:")
+            print(f"Mean number of defective bulbs per sample = {Ex}")
+            print(f"Reported variance of {Vx:.2f} per sample")
+            print(f"P(3 or more defective bulbs in sample)={1 - hg.cdf(3):.2%}")
+            print(f"P(2 or more defective bulbs in sample)={1 - hg.cdf(2):.2%}")
+            print(f"P(1 or more defective bulbs in sample)={1 - hg.cdf(1):.2%}")
+            print(f"P(No defective bulbs in sample) = {hg.cdf(1):.2%}")
+            print()
 
     return [P, Ex, Vx]
+
+
+hypergeo_info(x_LIST, N, k_LIST, n_LIST)
 
 
 def HyperData(N, n, k, T):
@@ -29,19 +71,20 @@ def HyperData(N, n, k, T):
 
     """
 
-    total = []
-    counter = 0
-
-    while counter < T:
-        counter += 1
-
-        total.append(
-            np.random.Generator.hypergeometric(k, N - k, n)
-        )  # returns draws from sample
-
-        # print(f'''There were {hypgeo} successful draws from trial {counter}.''')
-
-    return total
+    hypgeo = RNG.hypergeometric(k, N - k, n, T)
+    count = np.unique_counts(hypgeo)
+    return [hypgeo, count]
 
 
-print(HyperData(200, 30, 2, 1000))
+# print(HyperData(200, 30, 2, 1000))
+
+
+def histoplot(n, T):
+
+    plt.hist(HyperData(N, n, 2, T)[0], bins=np.linspace(0, 6), alpha=0.5)
+    plt.hist(HyperData(N, n, 4, T)[0], bins=np.linspace(0, 6), alpha=0.5)
+    plt.hist(HyperData(N, n, 6, T)[0], bins=np.linspace(0, 6), alpha=0.5)
+    plt.xlabel(f"number of defective bulbs found in sample of {n} bulbs")
+    plt.ylabel("frequency of result over 1000 trials")
+    plt.legend(["defect rate = 1%", "defect rate = 2%", "defect rate = 3%"])
+    plt.savefig(f"{n}_hist.jpg")
